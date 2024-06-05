bce5bdf8bece7aa0a103489c1d46d6f8ba16dc58
Daunoras <159426406+Daunoras@users.noreply.github.com>
2024-06-05 13:44:05 +0300

    Add files via upload

--- uzduotis/models/models.py
+++ uzduotis/models/models.py
@@ -2,25 +2,10 @@ from odoo import models, fields
 
 class Dokumentai(models.Model):
     _name = 'dokumentai'
-    _description = 'Dokumentu informacija'
+    _description = 'Dokumentų informacija'
 
     pavadinimas = fields.Char(required=True)
     aprasymas = fields.Text()
     imone = fields.Char()
-
-
-
-# class uzduotis(models.Model):
-#     _name = 'uzduotis.uzduotis'
-#     _description = 'uzduotis.uzduotis'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    sukure = fields.Many2one('res.users', string='Įrašo autorius', default=lambda self: self.env.user)
+    palaikymas = fields.Many2many('res.users', string='Prižiūri')
