8f851d276f50801f9a4658cbecf0a221d9d37563
Daunoras <159426406+Daunoras@users.noreply.github.com>
2024-06-06 05:33:54 +0300

    Add files via upload

--- uzduotis/models/models.py
+++ uzduotis/models/models.py
@@ -5,7 +5,7 @@ class Dokumentai(models.Model):
     _description = 'Dokumentų informacija'
 
     pavadinimas = fields.Char(required=True)
-    aprasymas = fields.Text()
-    imone = fields.Char()
+    aprasymas = fields.Text(string='Aprašymas')
+    imone = fields.Char(string='Įmonė')
     sukure = fields.Many2one('res.users', string='Įrašo autorius', default=lambda self: self.env.user)
     palaikymas = fields.Many2many('res.users', string='Prižiūri')
