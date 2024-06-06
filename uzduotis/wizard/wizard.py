bb55cfdba917595457b8ca289826cc16d49b7126
Daunoras <159426406+Daunoras@users.noreply.github.com>
2024-06-06 03:28:43 +0300

    Add files via upload

--- uzduotis/wizard/wizard.py
+++ uzduotis/wizard/wizard.py
@@ -10,22 +10,37 @@ class DokumentaiWizard(models.TransientModel):
 
     def action_wizard(self):
         domain = []
-        if self.start_date and not self.end_date:
-            domain.append(('create_date', '>=', self.start_date))
-        if self.end_date and not self.start_date:
-            domain.append(('create_date', '<=', self.end_date))
-        if self.start_date and self.end_date:
-            domain.append(())
-    #     if self.user_id:
-    #         domain.append(('sukure', '=', self.user_id.id))
-    #
+        if not self.user_id:
+            if self.start_date and not self.end_date:
+                domain.append(('create_date', '>=', self.start_date))
+            if self.end_date and not self.start_date:
+                domain.append(('create_date', '<=', self.end_date))
+            if self.start_date and self.end_date:
+                domain += ['&',
+                             ('create_date', '>=', self.start_date),
+                             ('create_date', '<=', self.end_date)]
+        else:
+            if not self.start_date and not self.end_date:
+                domain.append(('sukure', '=', self.user_id.id))
+            if self.start_date and not self.end_date:
+                domain += ['&',
+                             ('sukure', '=', self.user_id.id),
+                             ('create_date', '>=', self.start_date)]
+            if self.end_date and not self.start_date:
+                domain += ['&',
+                             ('sukure', '=', self.user_id.id),
+                             ('create_date', '<=', self.end_date)]
+            if self.start_date and self.end_date:
+                domain += ['&',
+                             ('sukure', '=', self.user_id.id),
+                             '&',
+                               ('create_date', '>=', self.start_date),
+                               ('create_date', '<=', self.end_date)]
 
-
-
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': 'Išfiltruoti Dokumentai',
-    #         'view_mode': 'tree,form',
-    #         'res_model': 'dokumentai',
-    #         'domain': domain,
-    #     }
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Išfiltruoti Dokumentai',
+            'view_mode': 'tree,form',
+            'res_model': 'dokumentai',
+            'domain': domain,
+        }
